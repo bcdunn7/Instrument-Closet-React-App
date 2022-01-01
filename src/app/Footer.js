b36a59cc10901738b2f273a6f9a7ecdbe3e58d9d
Bryce <bryce.cc.dunn@gmail.com>
2022-01-01 15:39:18 -0600

    Github footer links.

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -1,8 +1,26 @@
 import './Footer.css';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { Button } from '@mui/material';
 
 const Footer = () => {
     return(
-        <footer className='Footer'><b>T</b>he<b>I</b>nstrument<b>C</b>loset</footer>
+        <footer className='Footer'>
+            <span>
+                Backend 
+                <a href='https://github.com/bcdunn7/Instrument-Closet-Backend'>    
+                    <GitHubIcon className='Footer-icon' color='primaryDark' fontSize='inherit'/>
+                </a>
+            </span>
+            <span>
+                <b>T</b>he<b>I</b>nstrument<b>C</b>loset. 
+            </span>
+            <span>
+                Frontend 
+                <a href='https://github.com/bcdunn7/Instrument-Closet-React-App'>    
+                    <GitHubIcon className='Footer-icon' color='primaryDark' fontSize='inherit'/>
+                </a>
+            </span>
+        </footer>
     )
 }
 
